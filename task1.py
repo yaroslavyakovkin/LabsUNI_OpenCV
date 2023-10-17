a44618dde6f2597c16663e82bb19b15b343d7618
Jaroslav Jakovkin <yaroslav.y2010@ya.ru>
2023-10-18 02:45:14 +0300

    Test

--- task1.py
+++ task1.py
@@ -14,9 +14,9 @@ flags = {'COLOR': cv2.IMREAD_COLOR, 'GRAY': cv2.IMREAD_GRAYSCALE}
 for type, flag  in flags.items():
     img = cv2.imread(file, flag)
 
-    #imgf = cv2.blur(img, (50, 50), None, (10, 10))
+    imgf = cv2.blur(img, (50, 50), None, (10, 10))
     #imgf = cv2.boxFilter(img, -1, (80,80))
-    imgf = cv2.medianBlur(img, 15) #3-2(15) 3-1(35)
+    #imgf = cv2.medianBlur(img, 15) #3-2(15) 3-1(35)
     #imgf = cv2.GaussianBlur(img, (41, 41), 1000, 100)
     #imgf = cv2.bilateralFilter(img, 40, 300, 500) #3-1
     #imgf = cv2.filter2D(img, -1, np.ones((50,50),np.float32)/2500) #3-2(20,50) 3-1(50,50)
