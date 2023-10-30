e11c4dc8a7897d298202e5e8a22699e0d8eb4306
Jaroslav Jakovkin <yaroslav.y2010@ya.ru>
2023-10-30 22:51:26 +0300

    task41 complete

--- Lab4/task41.py
+++ Lab4/task41.py
@@ -1,37 +1,43 @@
 import cv2
 import numpy as np
-from win32api import GetSystemMetrics as gsm  # monitor resolution
 
 
-def fixresize(img, max):
-    r = max / img.shape[0]
-    dim = (int(img.shape[1] * r), max)
-    return cv2.resize(img, dim)
-
-def morph(image, method, kernel):
+def morph(image, method, kernel, iterations):
     s = 0
     for i in range(2):
-        if method.lower() == "размыкание" or s == 1:
-            new = cv2.erode(image, kernel, None, None, 5)
+        if method == "open" or s == 1:
+            new = cv2.erode(image, kernel, None, None, iterations)
             s , method = (2,'')
-        elif method.lower() == "замыкание" or s == 2:
-            new = cv2.dilate(image, kernel, None, None, 5)
+            cv2.imshow('erode', 255-new)
+        elif method == "close" or s == 2:
+            new = cv2.dilate(image, kernel, None, None, iterations)
             s , method = (1,'')
-    return new
-        
-#method = 'замыкание'            
-method = 'размыкание'           
-k = np.ones((2, 2), np.uint8)
-axis = 0
-img = cv2.imread("Lab4\\4-1.jpg")
-#tres, imgt = cv2.threshold(img, 255/2, 255, cv2.THRESH_BINARY)
-img_new = morph(img, method, k)
-img_new2 = cv2.morphologyEx(img, cv2.MORPH_CLOSE, k, None, 5) 
+            cv2.imshow('dilate', 255-new)       
+        image = new
+    return image
 
-if img.shape[0] > img.shape[1]:
-    axis = 1
-img_combine = np.concatenate((img, img_new, img_new2), axis)
+methods = {'close':cv2.MORPH_CLOSE, 'open':cv2.MORPH_OPEN}
+method = input('''1.Замыкание
+2.Размыкание
+Номер требуемой операции: ''')
+if method == '1': method = 'close'  
+elif method == '2': method = 'open'
 
-cv2.imshow(f"edited", fixresize(img_combine, gsm(1) - 50))
+f = cv2.FONT_ITALIC
+img = 255 - cv2.imread("Lab4\\4-1.jpg")           
+k = np.ones((2, 2))
+i = 5
+c = (255, 255, 255)
+img_new = morph(img, method, k, i)
+img_new2 = cv2.morphologyEx(img, methods[method], k, None, None, i)
+img_diff = cv2.absdiff(img_new, img_new2)
+img_diff = cv2.dilate(img_diff, k)
+
+img_combine = 255 - np.concatenate((cv2.putText(img, 'original', (30,30), f, 1,c,1),
+                              cv2.putText(img_new, 'my func', (30,30), f, 1,c,1),
+                              cv2.putText(img_new2, 'cv2 func', (30,30), f, 1,c,1),
+                              cv2.putText(img_diff, 'difference', (30,30), f, 1,c,1)),1)
+
+cv2.imshow(f"{method}", img_combine)
 if cv2.waitKey() == ord("s"):
-    cv2.imwrite(f"Lab4\\morph\\2_4-1.jpg", img_combine)
+    cv2.imwrite(f"Lab4\\morph\\{method}-4-1.jpg", img_combine)
