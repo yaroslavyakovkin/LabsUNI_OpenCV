a720f1e3b279aeac6bfe3c626f75e734b648b870
Jaroslav Jakovkin <yaroslav.y2010@ya.ru>
2023-10-31 00:02:33 +0300

    final final task41

--- Lab4/task41.py
+++ Lab4/task41.py
@@ -8,11 +8,9 @@ def morph(image, method, kernel, iterations):
         if method == "open" or s == 1:
             new = cv2.erode(image, kernel, None, None, iterations)
             s , method = (2,'')
-            cv2.imshow('erode', 255-new)
         elif method == "close" or s == 2:
-            new = cv2.dilate(image, kernel, None, None, iterations)
+            new = cv2.dilate(image, kernel, None, None, iterations)    
             s , method = (1,'')
-            cv2.imshow('dilate', 255-new)       
         image = new
     return image
 
@@ -24,9 +22,9 @@ if method == '1': method = 'close'
 elif method == '2': method = 'open'
 
 f = cv2.FONT_ITALIC
-img = 255 - cv2.imread("Lab4\\4-1.jpg")           
-k = np.ones((2, 2))
+img = 255 - cv2.imread("Lab4\\4-1.jpg")   
 i = 5
+k = np.ones((2, 2))
 c = (255, 255, 255)
 img_new = morph(img, method, k, i)
 img_new2 = cv2.morphologyEx(img, methods[method], k, None, None, i)
@@ -40,4 +38,4 @@ img_combine = 255 - np.concatenate((cv2.putText(img, 'original', (30,30), f, 1,c
 
 cv2.imshow(f"{method}", img_combine)
 if cv2.waitKey() == ord("s"):
-    cv2.imwrite(f"Lab4\\morph\\{method}-4-1.jpg", img_combine)
+    cv2.imwrite(f"Lab4\\morph\\{method}-{i}_4-1.jpg", img_combine)
