5c9289bf6944d4171cd7f948885d7bb2b88aff28
yaroslavyakovkin <yaroslav.y2010@ya.ru>
2023-12-15 23:44:50 +0300

    task72 complete

--- Lab7/task72.py
+++ Lab7/task72.py
@@ -1,50 +1,43 @@
-import cv2, math
+import cv2
 import numpy as np
-from win32api import GetSystemMetrics as gsm # monitor resolution
 
-def draw_line(rho, theta, img, color=(100, 0, 255), thickness=3):
-    cos_t = math.cos(theta)
-    sin_t = math.sin(theta)
-    x0 = cos_t * rho
-    y0 = sin_t * rho
-    pt1 = int(x0 - 1000 * sin_t), int(y0 + 1000 * cos_t)
-    pt2 = int(x0 + 1000 * sin_t), int(y0 - 1000 * cos_t)
-    cv2.line(img, pt1, pt2, color, thickness)
+def draw_line_P(x0, y0, x1, y1, img, color=(125, 0, 255), thickness=3):
+    cv2.line(img, (x0, y0), (x1, y1),color, thickness)
 
-#изменение разрешения с учетом соотношения изображения
-def fixresize(img, max = gsm(1)-100):
-    r = max / img.shape[0]
-    dim = (int(img.shape[1] * r), int(max))
-    return cv2.resize(img, dim)
-
+i=0
 cap = cv2.VideoCapture('Lab5\\testvideo.mp4')
-width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)/3)
-height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)/3)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 size = (width, height)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('Lab7\\results\\output.mp4', fourcc, 30.0, size)
-i=0
-float(i)
+
 while True:
     ret, frame = cap.read()
-    frame = cv2.resize(frame,(int(frame.shape[1]/3), int(frame.shape[0]/3)))
     if not ret:
         break
-    i+=0.1
+    
     frame=cv2.cvtColor(255-cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY),cv2.COLOR_GRAY2BGR)
-    contours = cv2.Canny(cv2.blur(frame, (5,5)), 25, 100, L2gradient=False)
-    lines = cv2.HoughLines(contours, 1.5, np.pi/45, 50)
+
+    cntrs = cv2.Canny(cv2.blur(frame, (5,5)), 25, 100, L2gradient=False)
+    cntrs = cv2.morphologyEx(cntrs,cv2.MORPH_CLOSE,np.ones((3,5)), iterations=3)
+    cntrs = cv2.dilate(cntrs,np.ones((5,3)), iterations=2)
+
+    lines = cv2.HoughLinesP(cntrs, 1, np.pi/180, 200, 50, 20)
     edit = np.copy(frame)
+    
     if lines is not None:
         for line in lines:
-            rho, theta = line[0]
-            draw_line(rho, theta, edit)
+            a, b, c, d = line[0]
+            draw_line_P(a, b, c, d, edit)
     
-    cv2.imshow('test', fixresize(edit)) 
+    cv2.imshow('test', edit)
     out.write(edit)
+
     key = cv2.waitKey(1)
     if key == ord('s'):
-        cv2.imwrite(f'Lab7\\results\\sho{i}ot.jpg', np.concatenate((frame, edit), axis=1))
+        cv2.imwrite(f'Lab7\\results\\sho{int(i)}ot.jpg', np.concatenate((frame, cntrs, edit), axis=1))
+        i+=1
     elif key == ord('q'):
         break
 
